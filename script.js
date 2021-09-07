e814e9cbae97d3238625c9058b661d92d43b6b66
brianorora <brianorora@gmail.com>
2021-09-07 18:47:14 +0300

    Add mobile menu popup modal

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  const hamburger = document.getElementById('hamburger-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const closeBtn = mobileMenu.querySelector('.close-btn');
+  closeBtn.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+  });
+
+  hamburger.addEventListener('click', () => {
+    mobileMenu.style.display = 'block';
+  });
+};
